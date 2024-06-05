bded06cc82d6898f1dd8df5e855a20d862914a71
Gustavo Guzman <gguzmanmori@gmail.com>
2024-06-04 23:30:30 -0500

    bottom nav terminado

--- app/src/main/java/com/example/pryandroidclinica/MainActivity.java
+++ app/src/main/java/com/example/pryandroidclinica/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.pryandroidclinica;
 
+import androidx.annotation.DrawableRes;
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import androidx.annotation.NonNull;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import com.example.pryandroidclinica.fragments.PageFragment;
+import com.example.pryandroidclinica.fragments.RecyclerViewFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String SELECTION = "SELECTION";
 
+    private BottomNavigationView bottomNavigationView;
     EditText username;
     EditText password;
     Button loginButton;
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupBottomMenu(savedInstanceState);
 
         username = findViewById(R.id.txtIngresarUsuario);
         password = findViewById(R.id.txtIngresarContraseña);
@@ -37,5 +42,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    private void setupBottomMenu(Bundle savedInstanceState) {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(this::onItemSelectedListener);
+
+        if (savedInstanceState == null) {
+            //displays the initial fragment when app starts
+            bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        } else {
+            //restores the item selected before rotating
+            bottomNavigationView.setSelectedItemId(savedInstanceState.getInt(SELECTION));
+        }
+
+        bottomNavigationView.getOrCreateBadge(R.id.nav_notificacion).setNumber(1000);
+        bottomNavigationView.getOrCreateBadge(R.id.nav_historial).setVisible(true);
+    }
+
+    private boolean onItemSelectedListener(MenuItem item) {
+        int itemId = item.getItemId();
+        if (itemId == R.id.nav_home) {
+            showPageFragment(R.drawable.home, R.string.title_home);
+            return true;
+        }
+        else if (itemId == R.id.nav_notificacion) {
+            showPageFragment(R.drawable.notificacion, R.string.title_notificacion);
+            return true;
+        } else if (itemId == R.id.nav_historial) {
+            showPageFragment(R.drawable.historia, R.string.title_historial);
+            return true;
+        } else if (itemId == R.id.nav_profile) {
+            showPageFragment(R.drawable.usuario, R.string.title_profile);
+            return true;
+        } else {
+            throw new IllegalArgumentException("item not implemented : " + item.getItemId());
+        }
+    }
+
+    private void showPageFragment(@DrawableRes int iconId, @StringRes int title) {
+        showFragment(PageFragment.newInstance(iconId), title);
+    }
+
+    private void showFragment(Fragment frg, @StringRes int title) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .setCustomAnimations(R.anim.bottom_nav_enter, R.anim.bottom_nav_exit)
+                .replace(R.id.container, frg)
+                .commit();
+        setTitle(title);
     }
 }
