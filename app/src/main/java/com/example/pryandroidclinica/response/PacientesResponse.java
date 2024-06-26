202a03ff9b5c957efa25676bb43aca031fffe2f3
FrankOcrospoma <frankocrospomaugaz@gmail.com>
2024-06-26 02:07:03 -0500

    bartolo presente

--- app/src/main/java/com/example/pryandroidclinica/response/PacientesResponse.java
+++ app/src/main/java/com/example/pryandroidclinica/response/PacientesResponse.java
@@ -1,14 +1,11 @@
 package com.example.pryandroidclinica.response;
 
-import com.example.pryandroidclinica.model.Paciente;
 import java.util.List;
 
 public class PacientesResponse {
     private boolean status;
     private List<Paciente> data;
     private String message;
-
-    // Getters y setters
 
     public boolean isStatus() {
         return status;
@@ -32,5 +29,79 @@ public class PacientesResponse {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public static class Paciente {
+        private int id;
+        private String nombreUsuario;
+        private String email;
+        private String contrasena;
+        private int estado;
+        private int estadoToken;
+        private String nombre;
+        private String apeCompleto;
+        private String fechaNac;
+        private String documento;
+        private int tipoDocumentoId;
+        private int sexo;
+        private String direccion;
+        private String telefono;
+
+        // Getters
+        public int getId() {
+            return id;
+        }
+
+        public String getNombreUsuario() {
+            return nombreUsuario;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public String getContrasena() {
+            return contrasena;
+        }
+
+        public int getEstado() {
+            return estado;
+        }
+
+        public int getEstadoToken() {
+            return estadoToken;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getApeCompleto() {
+            return apeCompleto;
+        }
+
+        public String getFechaNac() {
+            return fechaNac;
+        }
+
+        public String getDocumento() {
+            return documento;
+        }
+
+        public int getTipoDocumentoId() {
+            return tipoDocumentoId;
+        }
+
+        public int getSexo() {
+            return sexo;
+        }
+
+        public String getDireccion() {
+            return direccion;
+        }
+
+        public String getTelefono() {
+            return telefono;
+        }
     }
 }
